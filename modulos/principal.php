a55e08da506f8e5cd74fc3557a5d4810689f84ce
martinsillo <martinsillo.gamboa@gmail.com>
2015-04-06 16:34:42 -0500

    modified

--- modulos/principal.php
+++ modulos/principal.php
@@ -1,49 +1,111 @@
-     <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            Sistema Unico de Padr&oacute;n de Beneficiarios
-            <small>Version preeliminar en desarrollo</small>
-          </h1>
-          <ol class="breadcrumb">
-            <li class="active"><i class="fa fa-home"></i> Inicio</li>
-          </ol>
-        </section>
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+<h1>
+Sistema de Planeación
+<small>Dashboard</small>
+</h1>
+<ol class="breadcrumb">
+<li class="active"><i class="fa fa-home"></i> Inicio</li>
+</ol>
+</section>
 <!-- Main content -->
         <section class="content">
           <div class="row">
             <!-- left column -->
-            <div class="col-md-8">
-              <!-- general form elements -->
-              <div class="box box-success">
-                <div class="box-header">
-                  <h3 class="box-title">Bienvenido</h3>
-                </div><!-- /.box-header -->
-                <!-- form start -->
-                  <div class="box-body"><p>Lorem Ipsum</p></div>
-              </div><!-- /.box -->
+           <div class="col-md-3 col-sm-6 col-xs-12">
+              <div class="info-box">
+                <span class="info-box-icon bg-green"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Proyectos</span>
+                  <span class="info-box-number">0</span>
+                </div><!-- /.info-box-content -->
+              </div><!-- /.info-box -->
+            </div><!-- /.col -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+              <div class="info-box">
+                <span class="info-box-icon bg-green"><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Componentes</span>
+                  <span class="info-box-number">41,410</span>
+                </div><!-- /.info-box-content -->
+              </div><!-- /.info-box -->
+            </div><!-- /.col -->
 
+            <!-- fix for small devices only -->
+            <div class="clearfix visible-sm-block"></div>
 
-
-
-
-              <!-- Input addon -->
-
-
-            </div><!--/.col (left) -->
+            <div class="col-md-3 col-sm-6 col-xs-12">
+              <div class="info-box">
+                <span class="info-box-icon bg-green"><span class="glyphicon glyphicon-tasks" aria-hidden="true"></span></span>
+                <div class="info-box-content">
+                  <span class="info-box-text">Actividades</span>
+                  <span class="info-box-number">760</span>
+                </div><!-- /.info-box-content -->
+              </div><!-- /.info-box -->
+            </div><!-- /.col -->
             <!-- right column -->
-            <div class="col-md-4">
-              <!-- general form elements disabled -->
-              <div class="box box-success">
-                <div class="box-header">
-                  <h3 class="box-title">General Elements</h3>
+        <div class='col-md-4'>
+              <!-- USERS LIST -->
+              <div class="box box-danger">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Latest Members</h3>
+                  <div class="box-tools pull-right">
+                    <span class="label label-danger">8 New Members</span>
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                  </div>
                 </div><!-- /.box-header -->
-                <div class="box-body">
-                  lOREM iPSUM
+                <div class="box-body no-padding">
+                  <ul class="users-list clearfix">
+                    <li>
+                      <img src="dist/img/user1-128x128.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">Alexander Pierce</a>
+                      <span class="users-list-date">Today</span>
+                    </li>
+                    <li>
+                      <img src="dist/img/user8-128x128.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">Norman</a>
+                      <span class="users-list-date">Yesterday</span>
+                    </li>
+                    <li>
+                      <img src="dist/img/user7-128x128.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">Jane</a>
+                      <span class="users-list-date">12 Jan</span>
+                    </li>
+                    <li>
+                      <img src="dist/img/user6-128x128.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">John</a>
+                      <span class="users-list-date">12 Jan</span>
+                    </li>
+                    <li>
+                      <img src="dist/img/user2-160x160.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">Alexander</a>
+                      <span class="users-list-date">13 Jan</span>
+                    </li>
+                    <li>
+                      <img src="dist/img/user5-128x128.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">Sarah</a>
+                      <span class="users-list-date">14 Jan</span>
+                    </li>
+                    <li>
+                      <img src="dist/img/user4-128x128.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">Nora</a>
+                      <span class="users-list-date">15 Jan</span>
+                    </li>
+                    <li>
+                      <img src="dist/img/user3-128x128.jpg" alt="User Image"/>
+                      <a class="users-list-name" href="#">Nadia</a>
+                      <span class="users-list-date">15 Jan</span>
+                    </li>
+                  </ul><!-- /.users-list -->
                 </div><!-- /.box-body -->
-              </div><!-- /.box -->
-            </div><!--/.col (right) -->
+                <div class="box-footer text-center">
+                  <a href="javascript::" class="uppercase">View All Users</a>
+                </div><!-- /.box-footer -->
+              </div><!--/.box -->
+            </div><!-- /.col -->
           </div>   <!-- /.row -->
         </section><!-- /.content -->
       </div><!-- /.content-wrapper -->
