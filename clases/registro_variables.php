ad19ad261adeebd7f4c596a99c42e309afe93868
José Martín Gamboa Murillo <martirius@localhost.localdomain>
2015-04-06 14:45:15 -0500

    se agrega variable

--- clases/registro_variables.php
+++ clases/registro_variables.php
@@ -1,8 +1,10 @@
 <?php
-$ConsultaDatosUsuario = "SELECT id_usuario,dependencia FROM usuarios WHERE usuario = '$usr' AND clave = '$clave'";
+$ConsultaDatosUsuario = "SELECT id_usuario,dependencia,perfil FROM usuarios WHERE usuario = '$usr' AND clave = '$clave'";
 $ResultadoUsuarios = $conexion->query($ConsultaDatosUsuario);
 $Resultados = $ResultadoUsuarios->fetch_array();
 $_SESSION['id_usuario'] = $Resultados[0];
 $_SESSION['dependencia'] = $Resultados[1];
+$_SESSION['perfil'] = $Resultados[2];
 $_SESSION['activo']=true;
+$_SESSION['ejercicio'] = $_POST['ejercicio'];
 ?>
